05de5e07fe13e816fd00fbb45c9ba8399269fa50
Mike Boyle <mikesboyle@gmail.com>
2020-03-08 15:46:50 -0400

    01_node

---
+++ 01_node/fromScratch.js
@@ -0,0 +1,7 @@
+/**
+ * Make a linked list that spells the letters
+ * of your favorite food.
+ *
+ * Create your own Node class from scratch to do this!
+ * DO NOT import the Node class from ./node.js
+ */
---
+++ 01_node/makeList.js
@@ -0,0 +1,16 @@
+const { Node } = require('./node');
+
+/**
+ * Create a linked list with three nodes, A -> B -> C
+ * There are many ways you can do this!
+ */
+const list = new Node('A');
+// your code here
+
+console.log(JSON.stringify(list));
+
+/**
+ * Create a linked list that spells your first or last name.
+ * Example: K -> E -> L -> L -> Y
+ * (Why are double letters tricky?)
+ */
